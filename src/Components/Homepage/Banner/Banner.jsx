68029e766d47c3ad37bfb6811759a4af0988909b
Source-Code-007 <utsho926@gmail.com>
2023-08-06 13:36:46 +0600

    header complete

--- src/Components/Homepage/Banner/Banner.jsx
+++ src/Components/Homepage/Banner/Banner.jsx
@@ -1,9 +1,42 @@
 import React from 'react';
+import banner1 from '/public/assets/img/Homepage/DocEyeBanner.svg'
 
 const Banner = () => {
+    const services = [
+        {
+            title: 'Live Video Consultaion',
+            subTitle: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae quos impedit maxime. Quia, officiis aliquid!',
+            icon: 'icon'
+        },
+        {
+            title: 'Live Video Consultaion',
+            subTitle: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae quos impedit maxime. Quia, officiis aliquid!',
+            icon: 'icon'
+        },
+        {
+            title: 'Live Video Consultaion',
+            subTitle: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae quos impedit maxime. Quia, officiis aliquid!',
+            icon: 'icon'
+        },
+        {
+            title: 'Live Video Consultaion',
+            subTitle: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae quos impedit maxime. Quia, officiis aliquid!',
+            icon: 'icon'
+        },
+    ]
     return (
-        <div className='h-screen bg-orange-500'>
-            <h2>This is banner section!</h2>
+        <div className='h-screen bg-center' style={{ backgroundImage: `url(${banner1.src})` }}>
+            <div className='my-container pt-28 flex flex-col justify-between h-3/4 text-center'>
+                <h2 className='my-title'>Complete health solution DocWatch!</h2>
+                <p className='font-semibold text-lg text-slate-700 px-16'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum eius iste quis laborum cumque facilis, atque nulla quos veniam labore, modi excepturi? Expedita dolores magnam, qui non ex repudiandae sit.</p>
+                <div className='flex gap-5'>
+                    {services.map((service, ind) => <div key={ind} className='py-7 px-3 rounded bg-white space-y-4 text-center shadow-lg'>
+                        <span>{service.icon}</span>
+                        <h2 className='font-semibold text-2xl'>{service.title}</h2>
+                        <p>{service.subTitle.slice(0, 60)}</p>
+                    </div>)}
+                </div>
+            </div>
         </div>
     );
 };
--- src/Components/Homepage/Nav/Navbar.jsx
+++ src/Components/Homepage/Nav/Navbar.jsx
@@ -1,21 +1,41 @@
+'use client'
 import Link from 'next/link';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import logo from '/public/assets/img/logo.png'
 import Image from 'next/image';
 import MyMenu from '@/Components/HelpingCompo/MyMenu';
 
 const Navbar = () => {
+    const [isTop, setIsTop] = useState(true)
+
+    useEffect(()=>{
+        const isTopFunc = ()=>{
+            if(window.scrollY>100){
+                setIsTop(false)
+            } else{
+                setIsTop(true)
+            }
+        }
+        window.addEventListener('scroll', isTopFunc)
+        return()=>{
+            window.removeEventListener('scroll', isTopFunc)
+        }
+    }, [])
+
+
     return (
-        <nav className='bg-slate-200'>
+        <nav className={`${isTop? '' : 'bg-slate-100 shadow'} fixed left-0 right-0 top-0`}>
             <div className='py-4 my-container flex justify-between'>
                 <Link href={'/'}><Image height={40} width={40} src={logo} alt='DocEye'></Image></Link>
-                <ul className='flex gap-5 items-center text-slate-700 text-xl'>
+                <ul className='flex gap-10 items-center text-slate-700 text-xl'>
                     <MyMenu href={'/'}>Home</MyMenu>
                     <MyMenu href={'/about'}>About</MyMenu>
                     <MyMenu href={'/contact'}>Contact</MyMenu>
                     <MyMenu href={'/feature'}>Feature</MyMenu>
                     <MyMenu href={'/services'}>Services</MyMenu>
                 </ul>
+
+                <button className='my-btn-one'>Login</button>
             </div>
         </nav>
     );
