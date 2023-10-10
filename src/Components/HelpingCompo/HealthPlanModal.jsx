f039dd9c8a2180bf4a0507b794adc36af09e6828
Source-Code-007 <utsho926@gmail.com>
2023-10-10 14:33:04 +0600

    refactor

--- src/Components/HelpingCompo/HealthPlanModal.jsx
+++ src/Components/HelpingCompo/HealthPlanModal.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { FaCheck, FaXmark } from 'react-icons/fa6';
 
+
 const HealthPlanModal = ({ planForModal }) => {
     console.log(planForModal);
     const { planName, description, price, features } = planForModal
