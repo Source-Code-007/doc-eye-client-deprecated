e651b30fd09a2c169e85d06dff9e06119428aeb9
Source-Code-007 <utsho926@gmail.com>
2024-03-16 22:22:35 +0600

    get specialites isr added

--- src/GetApiData/getSpecialties.jsx
+++ src/GetApiData/getSpecialties.jsx
@@ -1,6 +1,10 @@
 const getSpecialties = async () => {
     try {
-        const res = await fetch(`${process.env.NEXT_PUBLIC_BACKEND_API}/admin/specialties`)
+        const res = await fetch(`${process.env.NEXT_PUBLIC_BACKEND_API}/admin/specialties`, {
+            next:{
+                revalidate: 20
+            }
+        })
         const data = await res.json()
         return data
     } catch (e) {
