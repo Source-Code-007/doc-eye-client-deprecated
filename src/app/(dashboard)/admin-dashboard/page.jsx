99f4fe3eee97d701b35a96d1c85acc1897c54189
Source-Code-007 <utsho926@gmail.com>
2024-04-03 13:27:05 +0600

    admin dashboard home modified

--- src/app/(dashboard)/admin-dashboard/page.jsx
+++ src/app/(dashboard)/admin-dashboard/page.jsx
@@ -14,12 +14,12 @@ const AdminDashboardHome = () => {
                         <span className='p-2 rounded-md bg-primary-main bg-opacity-20 border border-primary-main text-primary-main'><FaHandHoldingMedical /></span>
                         <div className='space-y-1'>
                             <h2 className='font-semibold text-md md:text-lg'>Total patient in this month</h2>
-                            <p className='text-primary-desc text-sm'>Total patient visited to your portal in this month</p>
+                            <p className='text-primary-desc text-xs sm:text-sm'>Total patient visited to your portal in this month</p>
                         </div>
                     </div>
-                    <div className='flex justify-between'>
-                        <h2 className='font-bold text-xl md:text-2xl'>1550</h2>
-                        <div className='flex items-center gap-1'>
+                    <div className='flex justify-between gap-3'>
+                        <h2 className='font-bold text-md md:text-lg lg:text-xl xl: text-2xl'>1550</h2>
+                        <div className='flex items-center gap-1 text-xs md:text-md'>
                             {/* TODO: Need condition to show this data */}
                             <span className='text-success-desc'><FaArrowTrendUp /></span> <span className='text-success-desc'>+32%</span> up from last months!
                         </div>
@@ -33,12 +33,12 @@ const AdminDashboardHome = () => {
                         <span className='p-2 rounded-md bg-secondary-main bg-opacity-20 border border-secondary-main text-secondary-main'><FaUserDoctor /></span>
                         <div className='space-y-1'>
                             <h2 className='font-semibold text-md md:text-lg'>Total doctor in this month</h2>
-                            <p className='text-primary-desc text-sm'>Total doctor join to your portal in this month</p>
+                            <p className='text-primary-desc text-xs sm:text-sm'>Total doctor join to your portal in this month</p>
                         </div>
                     </div>
-                    <div className='flex justify-between'>
-                        <h2 className='font-bold text-xl md:text-2xl'>16</h2>
-                        <div className='flex items-center gap-1'>
+                    <div className='flex justify-between gap-3'>
+                        <h2 className='font-bold text-md md:text-lg lg:text-xl xl: text-2xl'>16</h2>
+                        <div className='flex items-center gap-1 text-xs md:text-md'>
                             {/* TODO: Need condition to show this data */}
                             <span className='text-danger-desc'><FaArrowTrendDown /></span> <span className='text-danger-desc'>-13%</span> down from last months!
                         </div>
@@ -52,12 +52,12 @@ const AdminDashboardHome = () => {
                         <span className='p-2 rounded-md bg-success-desc bg-opacity-20 border border-success-desc text-success-desc'><FaFileMedical /></span>
                         <div className='space-y-1'>
                             <h2 className='font-semibold text-md md:text-lg'>Total appointment in this month</h2>
-                            <p className='text-primary-desc text-sm'>Total appointments scheduled for this month</p>
+                            <p className='text-primary-desc text-xs sm:text-sm'>Total appointments scheduled for this month</p>
                         </div>
                     </div>
-                    <div className='flex justify-between'>
-                        <h2 className='font-bold text-xl md:text-2xl'>1250</h2>
-                        <div className='flex items-center gap-1'>
+                    <div className='flex justify-between gap-3'>
+                        <h2 className='font-bold text-md md:text-lg lg:text-xl xl: text-2xl'>1250</h2>
+                        <div className='flex items-center gap-1 text-xs md:text-md'>
                             {/* TODO: Need condition to show this data */}
                             <span className='text-success-desc'><FaArrowTrendUp /></span> <span className='text-success-desc'>15%</span> down from last months!
                         </div>
@@ -71,12 +71,12 @@ const AdminDashboardHome = () => {
                         <span className='p-2 rounded-md bg-secondary-main-2 bg-opacity-20 border border-secondary-main-2 text-secondary-main-2'><GiProfit /></span>
                         <div className='space-y-1'>
                             <h2 className='font-semibold text-md md:text-lg'>Total revenue in this month</h2>
-                            <p className='text-primary-desc text-sm'>Total revenue earned from patients this month</p>
+                            <p className='text-primary-desc text-xs sm:text-sm'>Total revenue earned from patients this month</p>
                         </div>
                     </div>
-                    <div className='flex justify-between'>
-                        <h2 className='font-bold text-xl md:text-2xl'>$3250</h2>
-                        <div className='flex items-center gap-1'>
+                    <div className='flex justify-between gap-3'>
+                        <h2 className='font-bold text-md md:text-lg lg:text-xl xl: text-2xl'>$3250</h2>
+                        <div className='flex items-center gap-1 text-xs md:text-md'>
                             {/* TODO: Need condition to show this data */}
                             <span className='text-danger-desc'><FaArrowTrendDown /></span> <span className='text-danger-desc'>-5%</span> down from last months!
                         </div>
